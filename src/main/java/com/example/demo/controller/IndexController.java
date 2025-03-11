fbbce8f66c2ac850e39c1bb90493e0ca7e7b68ed
Glen02lee <glen@handong.ac.kr>
2025-03-11 19:52:43 +0900

    1st day

---
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -0,0 +1,33 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("/index") //컨트롤러가 찾아가는 주소라고 생각하면 된다.
+    @ResponseBody   // 페이지를 리턴해줘야하는데, json 형태로 리턴해주기 위한 코드
+    public String index(){
+        return "11222333";
+    }
+    @RequestMapping("/abc")
+    @ResponseBody
+    public Map<String, Object> abc() {
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("message", "haha!!");
+        return map; // JSON 형식으로 응답
+    }
+
+    // 일반 컨트롤러이고, 페이지를 리턴 해줄거야
+    @RequestMapping("/page")
+    @ResponseBody
+    public String page(){
+        return "page";
+    }
+}
+// 컨트롤러는 페이지와 레스트가 있다
