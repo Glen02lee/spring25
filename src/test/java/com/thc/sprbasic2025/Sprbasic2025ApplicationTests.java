b52dc568942ec122f040c54425b4e747378d08d7
Glen02lee <glen@handong.ac.kr>
2025-03-31 19:24:24 +0900

    add all

--- src/test/java/com/thc/sprbasic2025/Sprbasic2025ApplicationTests.java
+++ src/test/java/com/thc/sprbasic2025/Sprbasic2025ApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.thc.sprbasic2025;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class Sprbasic2025ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
